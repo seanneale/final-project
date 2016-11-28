53d97d7da77a5a62551ab9a01e75dbd3f3a8ebbc
seanneale <sean_neale@hotmail.co.uk>
2016-11-28 09:31:07 +0800

    updated match submission stops match not being run

--- app/controllers/api/match_stats_controller.rb
+++ app/controllers/api/match_stats_controller.rb
@@ -4,18 +4,21 @@ class Api::MatchStatsController < ApplicationController
 		params.each do |key,value|
 			game_team_id = params[:game_team_id]
 			if key < "99"
-				value.class
 				if GamePlayer.find(value.to_i).game_team.league.rounds.find_by(active: true).matches.find_by(home_team_id: game_team_id)
-					match_id = GamePlayer.find(value.to_i).game_team.league.rounds.find_by(active: true).matches.find_by(home_team_id: game_team_id)[:id]
-					Match.find(match_id).update(home_team_picked: true)
+					@match_id = GamePlayer.find(value.to_i).game_team.league.rounds.find_by(active: true).matches.find_by(home_team_id: game_team_id)[:id]
+					Match.find(@match_id).update(home_team_picked: true)
 				else 
-					match_id = GamePlayer.find(value.to_i).game_team.league.rounds.find_by(active: true).matches.find_by(away_team_id: game_team_id)[:id]
-					Match.find(match_id).update(away_team_picked: true)
+					@match_id = GamePlayer.find(value.to_i).game_team.league.rounds.find_by(active: true).matches.find_by(away_team_id: game_team_id)[:id]
+					Match.find(@match_id).update(away_team_picked: true)
 				end
-				GamePlayer.find(value.to_i).match_stats.create(match_id: match_id)
+				GamePlayer.find(value.to_i).match_stats.create(match_id: @match_id)
 			end
 		end
-		MatchWorker.perform_async()
+		# MatchWorker.perform_async()
+		Match.find(@match_id).round.matches.each do |match|
+			MatchWorker.perform_async(match[:id])
+			sleep(1)
+		end
 	end
 
 end
